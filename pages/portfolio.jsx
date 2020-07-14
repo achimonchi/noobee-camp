fca3b6cda79a744e47276f39c54fc8305325e5a5
NooBeeID <noobee@localhost.localdomain>
2020-07-14 22:24:53 +0700

    add portfolio and program pages

--- pages/portfolio.jsx
+++ pages/portfolio.jsx
@@ -1,17 +1,42 @@
 import Layout from '../components/Layout'
 
-class Portfolio extends React.Component{
 
-    constructor(props){
-        super(props)
-    }
-    render(){
-        return(
-            <Layout activeClass={this.props.url.pathname}>
-                <h1>Portfolio Peserta</h1>
-            </Layout>
-        )
-    }
+const _banner = () =>{
+    const date = new Date();
+    const day = date.getDay();
+    const listMonth=[
+        "January", "February", "March", "April", "May",
+        "June", "July", "August", "September", "October",
+        "November", "December"
+    ]
+    const month = listMonth[date.getMonth()];
+    const year = date.getFullYear();
+    return (
+        <div className="banner">
+            <div className="bg-banner">
+                <div className="container-fluid">
+                    <div className="row justify-content-center">
+                        <div className="col-md-7 text-center">
+                            <div className="banner-wrapper">
+                                <h1 className="mt-5 text-banner"><b>Kumpulan Portfolio Yang Telah Dibuat Oleh Peserta NooBee<span>Camp</span></b></h1>
+                                    <h4 className="sub-text-banner mt-4"><i>*Updated <b>{`${day}-${month}-${year}`}</b></i></h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+const Portfolio = (props) => {
+    return(
+        <Layout activeClass={props.url.pathname}>
+            <div className="wrapper">
+                <_banner/>
+            </div>
+        </Layout>
+    )
 } 
 
 
--- pages/program.jsx
+++ pages/program.jsx
@@ -2,6 +2,34 @@ import React, {useState, useEffect} from 'react'
 import Layout from '../components/Layout'
 import Link from 'next/link';
 
+const _banner = ({kelas}) =>{
+    const date = new Date();
+    const day = date.getDay();
+    const listMonth=[
+        "January", "February", "March", "April", "May",
+        "June", "July", "August", "September", "October",
+        "November", "December"
+    ]
+    const month = listMonth[date.getMonth()];
+    const year = date.getFullYear();
+    return (
+        <div className="banner">
+            <div className="bg-banner">
+                <div className="container-fluid">
+                    <div className="row justify-content-center">
+                        <div className="col-md-7 text-center">
+                            <div className="banner-wrapper">
+                                    <h1 className="mt-5 text-banner"><b>Kelas <span>{kelas}</span></b></h1>
+                                    <h4 className="sub-text-banner mt-4"><i>Program <b>Batch #1</b><br/>Periode <b>Oktober - Desember</b></i></h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
 const Program = (props) => {
     const [className, setClassName] = useState(props.url.query.kelas);
 
@@ -27,23 +55,29 @@ const Program = (props) => {
     }, [props.url.query.kelas])
     return(
         <Layout activeClass={props.url.pathname}>
-            <div className="container">
-                <div className="row text-center">
-                    {notFound 
-                    ? <div className="col-md">
-                        <h1 className="mt-3">404 </h1> 
-                        <h4 className="mt-4">Maaf.. Kelas yang anda cari, tidak ditemukan</h4>
-                        <h6>List kelas yang tersedia : </h6>
-                        <h6>
-                            {listClass.map((l,i)=>(
-                                <Link href={"/program?kelas="+l}><a className="mr-3" key={i}>{l}</a></Link>
-                                
-                            ))}
-                        </h6>
+            {notFound 
+                ?   <div className="wrapper bg-dark text-white p-5">
+                        <div className="container p-5">
+                            <div className="row text-center">
+                            <div className="col-md">
+                                <h1 className="mt-3">404 </h1> 
+                                <h4 className="mt-4">Maaf.. Kelas yang anda cari, tidak ditemukan</h4>
+                                <h6>List kelas yang tersedia : </h6>
+                                <h6>
+                                    {listClass.map((l,i)=>(
+                                        <Link href={"/program?kelas="+l}><a className="mr-3" key={i}>{l}</a></Link>
+                                        
+                                    ))}
+                                </h6>
+                            </div>            
+                            </div>
+                        </div>
                     </div>
-                    : <h1>Program Class {className}</h1> }
-                </div>
-            </div>
+                :   <div className="wrapper">
+                        <_banner
+                            kelas={className}
+                        />
+                    </div>}
         </Layout>
     )
 }
